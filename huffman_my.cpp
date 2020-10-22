5e2c34d6e91bf467df19aa7de7684fb62026a9ae
MrHause <mrhause90@gmail.com>
2020-10-22 19:52:03 +0200

    inital, add strucktures for tree

---
+++ huffman_my.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+typedef struct {
+	char sign;
+	uint16_t count;
+}Position;
+
+typedef struct {
+	Position position;
+	struct Node* next;
+	struct Node* left;
+	struct Node* right;
+}Node;
+
+
+
+int main()
+{
+	Node* tree;
+
+	string input_txt;
+	getline(cin, input_txt);
+
+
+    std::cout << "Hello World!\n";
+}
