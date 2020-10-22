2f95a4cef647c70199cd7f5e89350109834f9abe
MrHause <mrhause90@gmail.com>
2020-10-22 20:34:51 +0200

    dodanie zliczania wystąpień danych liter i tworzenie listy z węzłów

--- huffman_my.cpp
+++ huffman_my.cpp
@@ -8,14 +8,48 @@ typedef struct {
 	uint16_t count;
 }Position;
 
-typedef struct {
+typedef struct node{
 	Position position;
-	struct Node* next;
-	struct Node* left;
-	struct Node* right;
+	struct node* next;
+	struct node* left;
+	struct node* right;
 }Node;
 
 
+//create list that stores signs and qty
+void create_list(Node** node, string input_txt) {
+	*node = NULL;		//init first node, set it's address to nothing
+	Node* temp_node;
+
+	for (uint32_t i = 0; i < input_txt.length(); i++) 
+	{
+		temp_node = *node;                
+		while (temp_node && (temp_node->position.sign != input_txt[i]))
+			temp_node = temp_node->next; 
+		if (!temp_node)                     
+		{
+			temp_node = new Node;             
+			temp_node->next = *node;            
+			temp_node->left = NULL;            
+			temp_node->right = NULL;
+			temp_node->position.sign = input_txt[i];
+			temp_node->position.count = 0;
+			*node = temp_node;               
+		}
+		temp_node->position.count++;                   
+	}
+}
+
+void Node_free(Node **list)
+{
+	Node* wsk;
+	while ( *list != NULL )
+	{
+		wsk = (*list)->next;
+		delete *list;
+		*list = wsk;
+	}
+}
 
 int main()
 {
@@ -24,6 +58,8 @@ int main()
 	string input_txt;
 	getline(cin, input_txt);
 
+	create_list( &tree, input_txt );
 
+	Node_free( &tree );
     std::cout << "Hello World!\n";
 }
